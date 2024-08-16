d8fea35163c8bccb3ec464094c3c8c5749253596
John Coene <jcoenep@gmail.com>
2024-08-16 21:27:20 +0200

    improve roadmap

--- src/components/Roadmap/index.js
+++ src/components/Roadmap/index.js
@@ -5,24 +5,40 @@ import styles from './styles.module.css';
 const Items = [
   {
     entry: 'R type inference',
+    description: 'Infer types from base R so type checking can be executed.',
   },
   {
-    entry: 'Autocompletion LSP',
+    entry: 'Complete LSP',
+    description: 'The LSP currently only provides diagnostics, it should also provide autocompletion, help pages, definitions, and more.',
   },
   {
     entry: 'Vapour type inference',
+    description: 'Infer types within Vapour to slightly reduce type declarations.',
   },
   {
     entry: 'Vapour package development tools',
+    description: 'Vapour could probably provide functionalities similar to those provided by devtools.',
   },
   {
     entry: 'Improve transpiler',
+    description: 'The transpiler currently outputs valid but extremely ugly R code.',
+  },
+  {
+    entry: 'Formatter',
+    description: 'Include a formatter.',
+  },
+  {
+    entry: 'Extensions',
+    description: 'Allow extending Vapour, e.g.: with @decorators',
   },
 ];
 
-function Item({entry}) {
+function Item({entry, description}) {
   return (
-    <p className={clsx(styles.item)}>{entry}</p>
+    <div className={clsx(styles.item)}>
+      <p className={clsx(styles.title)}>{entry}</p>
+      <p>{description}</p>
+    </div>
   );
 }
 
